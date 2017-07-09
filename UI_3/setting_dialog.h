056cb305f9e24fe065f2178d8e9964682ee4e568
HK-IWTR <1433612902@qq.com>
2017-07-09 11:49:26 +0800

    UI_3
    2017-7-9
    拼图程序的第三代UI，完成了将图片增加到Button上的功能

---
+++ UI_3/setting_dialog.h
@@ -0,0 +1,45 @@
+#ifndef SETTING_DIALOG_H
+#define SETTING_DIALOG_H
+
+#include <QDialog>
+#include <QFileDialog>
+
+namespace Ui {
+class Setting_Dialog;
+}
+
+class Setting_Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Setting_Dialog(QWidget *parent = 0);
+    ~Setting_Dialog();
+
+private slots:
+    void on_pushButton_clicked();
+
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+private:
+    Ui::Setting_Dialog *ui;
+    QFileDialog fileDlg;
+
+
+    QString ButtonStyle_1 = "QPushButton{background-color:#907B63;"
+
+                    "color: white;   border-radius: 10px;  border: 2px groove gray;"
+
+                    "border-style: outset;}"
+
+                    "QPushButton:hover{background-color:white; color: black;}"
+
+                    "QPushButton:pressed{background-color:rgb(85, 170, 255);"
+
+                    "border-style: inset; }";
+};
+
+#endif // SETTING_DIALOG_H
